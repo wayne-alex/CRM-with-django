f58ec763dd18fc6b3c0589667ff0fc38973b6914
Wayne Alex <waynealex9914@gmail.com>
2023-07-05 20:32:39 +0300

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    port = '3306',
+)
+
+mydb = database.cursor()
+
+mydb.execute('CREATE DATABASE captain')
+print("Database created!")
